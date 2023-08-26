2734adcb2c47b6c6eba73a63e74d48e745bc1c51
Subo2002 <subocashell@gmail.com>
2023-08-27 00:57:51 +0100

    fixed movement to fit for isometric

--- Player.gd
+++ Player.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
-@export var speed : float = 100;
+@export var speed : float = 100
+@export var gravity:float = 980
 
 func _physics_process(delta):
 	#Input Direction
 	var input_direction : Vector2 = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
+		(Input.get_action_strength("down") - Input.get_action_strength("up"))/2
 	)
-	velocity = input_direction*speed
-	move_and_slide()
+	velocity = input_direction.normalized()*speed
+	position += velocity * delta
 	
